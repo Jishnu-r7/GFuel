682199926acfa49dd3f7c15a23763937fbce1252
Jishnu R <79126407+Jishnu-r7@users.noreply.github.com>
2024-06-22 02:02:12 +0530

    Update app.py

--- LPR/app.py
+++ LPR/app.py
@@ -13,7 +13,7 @@ model.conf = 0.25  # NMS confidence threshold
 model.iou = 0.45  # NMS IoU threshold
 model.agnostic = False  # NMS class-agnostic
 model.multi_label = False  # NMS multiple labels per box
-model.max_det = 1000  # maximum number of detections per image
+model.max_det = 2  # maximum number of detections per image
 
 reader = easyocr.Reader(['en'])
 
